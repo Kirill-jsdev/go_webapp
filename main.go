8c0711a839ecbe4153c1c86558778c5a9c552b5c
Kirill Zlobin <jsdevzk@gmail.com>
2025-05-08 11:46:32 +0500

    Rendered basic HTML templates with a built-in standard library

--- main.go
+++ main.go
@@ -1,51 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "Hello, world!")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Number of bytes written: %d\n", n)
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "About!")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Number of bytes written: %d\n", n)
+	renderTemplate(w, "about.page.tmpl")
+
 }
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 10.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
+		fmt.Println("error parsing template:", err)
 		return
 	}
-	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 10.0, f)
 }
+
+
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Starting app on port: %s\n", portNumber)
 	http.ListenAndServe(portNumber, nil)
