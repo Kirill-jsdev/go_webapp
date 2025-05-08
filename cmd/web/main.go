a6c8b188c54c75ddd161e27f8c4296f112cbd366
Kirill Zlobin <jsdevzk@gmail.com>
2025-05-08 13:23:19 +0500

    Restructured into multiple packages

---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"myapp/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+
+
+
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf("Starting app on port: %s\n", portNumber)
+	http.ListenAndServe(portNumber, nil)
+}
